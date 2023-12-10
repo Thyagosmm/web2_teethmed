64893e3ef5fa1d385969bb1b1b4648b839035d41
silvamelo510 <thyago.smm@gmail.com>
2023-12-10 16:56:29 -0300

    Consultando, excluindo e criando views

--- server.js
+++ server.js
@@ -8,11 +8,8 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(express.static("public"));
 const Paciente = require("./model/my_db");
 
-app.get("/inicio", (req, res) => {
-  res.send(
-    "<h1>Você está na página inicial da clínica odontológica TeethMed!</h1>"
-  );
-});
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 app.post("/agendamentoservico", urlencodedParser, (req, res) => {
   let htmlResponse = "<h1>Comprovante de Agendamento</h1><table>";
@@ -23,7 +20,7 @@ app.post("/agendamentoservico", urlencodedParser, (req, res) => {
   res.send(htmlResponse);
 });
 
-app.post("/cadastropaciente", urlencodedParser, (req, res) => {
+app.post("/cadastrarpaciente", urlencodedParser, (req, res) => {
   var nomePaciente = req.body.nome;
   var idadePaciente = req.body.idade;
   var cpfPaciente = req.body.cpf;
@@ -39,6 +36,87 @@ app.post("/cadastropaciente", urlencodedParser, (req, res) => {
     .catch(function (erro) {
       res.send("Erro ao cadastrar o paciente: " + erro);
     });
+});
+
+app.post("/buscarpaciente", urlencodedParser, (req, res) => {
+  var nomePaciente = req.body.nome;
+  var idadePaciente = req.body.idade;
+  var cpfPaciente = req.body.cpf;
+
+  Paciente.findOne({
+    where: { nome: nomePaciente, idade: idadePaciente, cpf: cpfPaciente },
+  })
+    .then(function (paciente) {
+      if (paciente) {
+        res.render("resultadobusca", { paciente: paciente });
+      } else {
+        res.send("Paciente não encontrado");
+      }
+    })
+    .catch(function (erro) {
+      res.send("Erro ao buscar o paciente: " + erro);
+    });
+});
+app.get("/alterarpaciente", (req, res) => {
+  var cpfPaciente = req.query.cpf;
+
+  Paciente.findOne({ where: { cpf: cpfPaciente } })
+    .then(function (paciente) {
+      if (paciente) {
+        res.render("alterar", { paciente: paciente });
+      } else {
+        res.send("Paciente não encontrado");
+      }
+    })
+    .catch(function (erro) {
+      res.send("Erro ao buscar o paciente: " + erro);
+    });
+});
+app.post("/alterarpaciente", urlencodedParser, (req, res) => {
+  var nomePaciente = req.body.nome;
+  var idadePaciente = req.body.idade;
+  var cpfPaciente = req.body.cpf;
+
+  Paciente.update(
+    { nome: nomePaciente, idade: idadePaciente },
+    { where: { cpf: cpfPaciente } }
+  )
+    .then(function ([rowsUpdate]) {
+      if (rowsUpdate === 0) {
+        res.send("Paciente não encontrado");
+      } else {
+        res.send("Dados do paciente atualizados com sucesso!");
+      }
+    })
+    .catch(function (erro) {
+      res.send("Erro ao atualizar os dados do paciente: " + erro);
+    });
+});
+app.get("/excluirpaciente", (req, res) => {
+  var cpfPaciente = req.query.cpf;
+
+  Paciente.destroy({
+    where: {
+      cpf: cpfPaciente,
+    },
+  })
+    .then(function () {
+      res.send("Paciente excluído com sucesso!");
+    })
+    .catch(function (erro) {
+      console.log("Erro na exclusão: " + erro);
+      res.send("Erro na exclusão" + erro);
+    });
+});
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/busca", (req, res) => {
+  res.render("busca");
+});
+app.get("/cadastro", (req, res) => {
+  res.render("cadastro");
 });
 
 app.listen(port, () => {
